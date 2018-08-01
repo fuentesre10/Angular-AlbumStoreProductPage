bb99f91e2576d94860e8b91e84c41298fddd3932
Ramiro Fuentes <rfuentes@swbc.com>
2018-08-01 16:31:41 -0500

    Corrections to component calling service.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,10 +4,10 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  private _albumUrl = "../assets/album.json";
+  private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
   public getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json);
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }  
 }
